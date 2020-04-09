e2fd78a194d2ff3ee0aebcd35932d9e5f443c0d1
Michał Szóstak <mszostakk01@gmail.com>
2020-04-09 21:28:27 +0200

    blogpost #1

---
+++ build/precache-manifest.8f8ac37ed034fb325150448aa6f73867.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6f74fdfb85a3935da51e0443f5b3a151",
+    "url": "/index.html"
+  },
+  {
+    "revision": "21185841e0d9cc5efd0b",
+    "url": "/static/css/main.601339b2.chunk.css"
+  },
+  {
+    "revision": "2f6473744d8845d434c2",
+    "url": "/static/js/2.214b9993.chunk.js"
+  },
+  {
+    "revision": "40046c021c0025c71b5ec67323b92a85",
+    "url": "/static/js/2.214b9993.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "21185841e0d9cc5efd0b",
+    "url": "/static/js/main.d9066bf5.chunk.js"
+  },
+  {
+    "revision": "45ea46f1d78188a12723",
+    "url": "/static/js/runtime-main.46d682fe.js"
+  },
+  {
+    "revision": "402635032303efd73912ffcf4f3f4235",
+    "url": "/static/media/project_manager.40263503.md"
+  }
+]);
